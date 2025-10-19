53812053e7b78da62946868efdbce7985a2128d2
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-10-19 15:04:20 -0400

    Time: 31 ms (18.94%) | Memory: 24.6 MB (84.48%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count_dict = {}
+        for num in nums:
+            if count_dict.get(num) == 1:
+                return True
+            count_dict[num] = 1
+        
+        return False
+
+
+        
