a5fe2d0f1647235a663eff33f5b5407aded9bf54
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-10-19 16:50:15 -0400

    Time: 15 ms (35.97%) | Memory: 20.1 MB (5.94%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        dict_nums = {}
+        for num in nums:
+            dict_nums[num] = dict_nums.get(num, 0) + 1
+        
+        # +1 here because each index corresponds to a frequency
+        # max freq value = len(num), so must have an index = len(num)
+        freq_list = [[] for i in range(len(nums) + 1)]
+        for num, freq in dict_nums.items():
+            freq_list[freq].append(num)
+        
+        res = []
+        for i in range(len(freq_list) - 1, 0, -1):
+            for num in freq_list[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+            
+
+
+        
