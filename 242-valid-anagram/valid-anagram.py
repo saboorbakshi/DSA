fdf637ef34527853af2c50b8535034be5be9f627
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-10-19 15:17:56 -0400

    Time: 15 ms (85.75%) | Memory: 12.6 MB (68.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        dict_s = {}
+        dict_t = {}
+
+        for char in s:
+            dict_s[char] = dict_s.get(char, 0) + 1
+        
+        for char in t:
+            dict_t[char] = dict_t.get(char, 0) + 1
+
+        if dict_s == dict_t:
+            return True
+        return False
+        
+        
+
+        
