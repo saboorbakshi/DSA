36099f28b14d0e2783785b711ea3cf440ffbf7fa
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-10-19 16:54:37 -0400

    Time: 13 ms (41.71%) | Memory: 20.2 MB (5.94%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,16 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # create dictionary of num -> freq
         dict_nums = {}
         for num in nums:
             dict_nums[num] = dict_nums.get(num, 0) + 1
         
+        # create array of freq -> [num]
         # +1 here because each index corresponds to a frequency
         # max freq value = len(num), so must have an index = len(num)
         freq_list = [[] for i in range(len(nums) + 1)]
         for num, freq in dict_nums.items():
             freq_list[freq].append(num)
         
+        # iterate over arry and return first k elements
         res = []
         for i in range(len(freq_list) - 1, 0, -1):
             for num in freq_list[i]:
