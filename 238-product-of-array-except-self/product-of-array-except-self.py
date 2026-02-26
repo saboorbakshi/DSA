da23eeda310c1a45c0cd0c27a8a616b2ca62de9f
Saboor Bakshi <saboor.bakshi@icloud.com>
2026-02-26 14:39:26 -0500

    Time: 55 ms (14.44%) | Memory: 22.3 MB (26.99%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        length = len(nums)
-        prefixes = [1] * length #left
-        suffixes = [1] * length #right
-        for i in range(1, length):
-            j = length - i - 1
+        n = len(nums)
+        prefixes = [1] * n #left
+        suffixes = [1] * n #right
+        for i in range(1, n):
+            j = n - i - 1
             prefixes[i] = nums[i - 1] * prefixes[i - 1]
             suffixes[j] = nums[j + 1] * suffixes[j + 1]
             # if i == 0:
             #     prefixes[i] = 1
             #     suffixes[i] = 1
-        return [prefixes[i] * suffixes[i] for i in range(length)]
+        return [prefixes[i] * suffixes[i] for i in range(n)]
         
         
 
