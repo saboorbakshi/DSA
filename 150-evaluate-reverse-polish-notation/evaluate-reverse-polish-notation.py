20de62c10dc163a8873f37457bf382b683f34064
Saboor Bakshi <saboor.bakshi@icloud.com>
2026-02-27 00:42:22 -0500

    Time: 20 ms (5.12%) | Memory: 20.9 MB (9.16%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        ops = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": lambda a, b: int(a/b)
+        }
+
+        stack = deque()
+        for idx, token in enumerate(tokens):
+            if token not in ops:
+                stack.append(int(token))
+            else:
+                # b = second_operand and a = first_operand
+                b = stack.pop()
+                a = stack.pop()
+                val = ops[token](a, b)
+                stack.append(val) # add it to the stack!
+                print(a)
+                print(token)
+                print(b)
+                print("-------------")
+                print(val)
+                print("#############")
+        return stack[0]
