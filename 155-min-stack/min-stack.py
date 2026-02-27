8c89b137c06e528dfdb87179a12098122bfcd548
Saboor Bakshi <saboor.bakshi@icloud.com>
2026-02-26 22:50:29 -0500

    Time: 10 ms (52.45%) | Memory: 16 MB (34.09%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,47 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.min = deque()
+        self.stack = deque()
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if len(self.min) < 1:
+            self.min.append(val)
+        else:
+            self.min.append(min(self.min[-1], val))
+        self.stack.append(val)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
